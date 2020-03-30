52b254197212fe511b05ba45a4dc95b1c05c6523
Nathan Keller <keller.nathan@gmail.com>
2020-03-30 10:03:25 -0600

    Trying to get the url to work in postman before anything

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov/api/sweet+potatoes/v1/nearest.json?api_key=OLG4ATJWCkL70Wp06zjfstNvM6hefUHiyYbhJLdf&location=Denver+CO")
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/generalSearchInput=sweet+potatos?api_key=OLG4ATJWCkL70Wp06zjfstNvM6hefUHiyYbhJLdf")
 
     require "pry"; binding.pry
   end
