b50f23647ad229bfed838ba82526972b0adb3294
Nathan Keller <keller.nathan@gmail.com>
2020-03-30 10:02:35 -0600

    Still trying to make a successful API call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-    
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/sweet+potatoes/v1/nearest.json?api_key=OLG4ATJWCkL70Wp06zjfstNvM6hefUHiyYbhJLdf&location=Denver+CO")
+
+    require "pry"; binding.pry
   end
 end
