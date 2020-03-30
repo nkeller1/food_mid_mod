fe4442ec7f62ac8acf4df21916492108c3fe1ddb
Nathan Keller <keller.nathan@gmail.com>
2020-03-30 10:21:23 -0600

    I can get the results in postman but not through faraday

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/generalSearchInput=sweet+potatos?api_key=OLG4ATJWCkL70Wp06zjfstNvM6hefUHiyYbhJLdf")
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/search?generalSearchInput=sweet%20potatoes&api_key=OLG4ATJWCkL70Wp06zjfstNvM6hefUHiyYbhJLdf")
 
     require "pry"; binding.pry
   end
